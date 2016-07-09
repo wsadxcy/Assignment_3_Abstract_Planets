a0d3037aa4960085db5378c9ab749c6da3d5668e
Chenyuan Xie <1147719157xie@gmail.com>
2016-07-08 20:54:31 -0400

    * Version : 0.8 -  *                  Finished Part 4,5,6  *                  Added IHabitable, IHasMoons, IHasRings Interface

--- Assignment_3_Abstract_Planet/Program.cs
+++ Assignment_3_Abstract_Planet/Program.cs
@@ -7,9 +7,9 @@ using System.Threading.Tasks;
  * Date : Jul 7th, 2016
  * Date Modified: Jul 8th, 2016
  * Description : Assignment 3
- * Version : 0.7 - 
- *                  Finished Part 3
- *                  Added HasMoons() and Habitable() Method
+ * Version : 0.8 - 
+ *                  Finished Part 4,5,6
+ *                  Added IHabitable, IHasMoons, IHasRings Interface
  */
 namespace Assignment_3_Abstract_Planet
 {
